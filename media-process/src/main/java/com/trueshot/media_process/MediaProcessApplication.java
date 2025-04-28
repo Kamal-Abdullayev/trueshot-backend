e4ea5045875d6efe2eb2d5b83cbb9ca45dbb477a
Kamal-Abdullayev <kamalabdu2layev@gmail.com>
2025-04-28 19:39:35 +0300

    project structure

---
+++ media-process/src/main/java/com/trueshot/media_process/MediaProcessApplication.java
@@ -0,0 +1,13 @@
+package com.trueshot.media_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaProcessApplication.class, args);
+	}
+
+}
---
+++ media-process/src/test/java/com/trueshot/media_process/MediaProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trueshot.media_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
