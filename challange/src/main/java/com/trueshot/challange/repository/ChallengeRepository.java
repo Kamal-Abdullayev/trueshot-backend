1e1bfb1c2e124e46cb623ad44da34d339f7576d4
RohanIyer136 <rohansriramiyer@gmail.com>
2025-05-01 15:09:33 +0400

    challenges microservices creating genral code

---
+++ challange/src/main/java/com/trueshot/challange/repository/ChallengeRepository.java
@@ -0,0 +1,8 @@
+package com.trueshot.challange.repository;
+
+import com.trueshot.challange.entity.Challenge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+    
+}
