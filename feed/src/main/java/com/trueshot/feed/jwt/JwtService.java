4e85b3877cb80fdb6ff296d04fb097be1d137a77
Feray Gulu-zada <fegulu@taltech.ee>
2025-05-04 21:39:11 +0300

    feed update

--- feed/src/main/java/com/trueshot/feed/jwt/JwtService.java
+++ feed/src/main/java/com/trueshot/feed/jwt/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -12,10 +13,11 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET = "4D6351665468576D5A7134743777217A25432A462D4A614E645267556B586E32";
+    @Value("${application.security.jwt.secret-key}")
+    private String secret;
 
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
